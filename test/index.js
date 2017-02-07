38c018d5cecba933681c6edd4c7bdc81070d51d9
Nik Coughlin <nrkn.com@gmail.com>
2017-02-07 16:49:33 +1300

    WIP

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ const assert = require( 'assert' )
 
 const testData = require( './fixtures/test.json' )
 
-const { toTree, toJson, pathFromNode, nodeFromPath } = require( '../src' )
+const { toTree, toJson } = require( '../src' )
 
 describe( '1tree/json converter', () => {
   testData.forEach( el => {
@@ -17,16 +17,6 @@ describe( '1tree/json converter', () => {
       const roundTrippedJsonStr = JSON.stringify( dataBackToJson )
 
       assert.equal( originalJsonStr, roundTrippedJsonStr )
-    })
-
-    it( 'can get paths and find paths for ' + originalJsonStr, () => {
-      const tree = toTree( el )
-      tree.walk( n => {
-        const nodePath = n.getPath()
-        const nodeFromPath = tree.atPath( nodePath )
-
-        assert.equal( n.get(), nodeFromPath.get() )
-      })
     })
   })
 })
