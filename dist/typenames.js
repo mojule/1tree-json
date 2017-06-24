a29d3a6d9a300c1024cb55be6d6d1e27719d25d2
Nik Coughlin <nrkn.com@gmail.com>
2017-06-24 13:22:21 +1200

    json-tree upgrade wip

--- dist/typenames.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var valueTypes = ['string', 'number', 'boolean'];
-var containerTypes = ['object', 'array'];
-var allTypes = valueTypes.concat(containerTypes).concat(['null']);
-
-module.exports = { valueTypes: valueTypes, containerTypes: containerTypes, allTypes: allTypes };
